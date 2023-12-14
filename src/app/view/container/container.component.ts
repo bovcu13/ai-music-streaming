3baade4d60ea1fa1c92701eec35bf10dbe64099b
一萱 <jjps30516@gmail.com>
2023-12-14 10:20:29 +0800

    feat: menu-grid

--- src/app/view/container/container.component.ts
+++ src/app/view/container/container.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 
 import { PRIMENG_MODULES } from "../../share/primeng";
+import { MenuComponent } from "../component/menu/menu.component";
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    PRIMENG_MODULES
-  ],
+    imports: [
+        RouterOutlet,
+        PRIMENG_MODULES,
+        MenuComponent
+    ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
