afd28079be9187089971556985cda06202d90275
一萱 <jjps30516@gmail.com>
2023-12-14 12:25:06 +0800

    feat: content-header

--- src/app/view/container/container.component.ts
+++ src/app/view/container/container.component.ts
@@ -3,15 +3,17 @@ import { RouterOutlet } from "@angular/router";
 
 import { PRIMENG_MODULES } from "../../share/primeng";
 import { MenuComponent } from "../component/menu/menu.component";
+import { HeaderComponent } from "../component/header/header.component";
 
 @Component({
   selector: 'app-container',
   standalone: true,
-    imports: [
-        RouterOutlet,
-        PRIMENG_MODULES,
-        MenuComponent
-    ],
+  imports: [
+    RouterOutlet,
+    PRIMENG_MODULES,
+    MenuComponent,
+    HeaderComponent
+  ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
