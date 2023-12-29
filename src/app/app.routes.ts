8adfca3c5e0f09ce97f65d97cbab8b4205f2ceb5
一萱 <jjps30516@gmail.com>
2023-12-29 11:19:12 +0800

    feat: page-fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from "./view/container/home/home.component";
-import { MusicPlayListComponent } from "./view/container/music-play-list/music-play-list.component";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'watch/:id', component: MusicPlayListComponent}
+  { path: '', loadChildren: () => import('./view/container/container.routes').then(m => m.routes) },
 ];
---
+++ src/app/view/container/container.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { MusicPlayListComponent } from "./music-play-list/music-play-list.component";
+import { ContainerComponent } from "./container.component";
+
+export const routes: Routes = [
+  {
+    path: '', component: ContainerComponent, children: [
+      { path: '', component: HomeComponent },
+      { path: 'watch/:id', component: MusicPlayListComponent }
+    ]
+  },
+];
