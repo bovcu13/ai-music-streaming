203998779c249b48fde3fbac2ed3c33e421fcf9c
一萱 <jjps30516@gmail.com>
2024-01-03 11:27:09 +0800

    feat: course

---
+++ src/app/share/data/course.ts
@@ -0,0 +1,38 @@
+export const course = [
+  {
+    imageUrl: 'https://img-c.udemycdn.com/course/480x270/3376336_444a_9.jpg',
+    audioElement: new Audio('https://www.chosic.com/wp-content/uploads/2022/01/Evening-Improvisation-with-Ethera(chosic.com).mp3'),
+    course_name: '2024 網頁全端開發',
+    tags: '暢銷課程',
+    total_hours: 80.5,
+    level: '所有級別',
+    summary: '超過80小時的內容，學習HTML, CSS, JavaScript, React, Next.js 以及 NodeJS, Express, MongoDB, Restful API，透過9個實作專案豐富作品集，透徹學習網頁開發的知識與技巧!',
+    highlights: [
+      '學習所有前端和後端開發的必要知識與技能，轉職成為前端或全端工程師',
+      '創造有形與富有現代設計感的優質網站，增加網站曝光度與實用性',
+      '學習HTML5語法，以及HTML Semantic Elements，並且使用HTML Validator驗證程式碼正確性，製作出功能性完整的網頁'
+    ],
+    rating: 4.8,
+    price: 330,
+    created_by: 'John Doe',
+    updated_at: '2024-01-01',
+  },
+  {
+    imageUrl: 'https://img-c.udemycdn.com/course/480x270/4885958_8992_7.jpg',
+    audioElement: new Audio('https://www.chosic.com/wp-content/uploads/2023/06/Moonset(chosic.com).mp3'),
+    course_name: '2024 Python全攻略',
+    tags: '暢銷課程',
+    total_hours: 45,
+    level: '所有級別',
+    summary: '超過40小時的內容，學習Python語法、程式邏輯、識別Python資料類別與使用時機，用正規表示式處理資料、處理文件系統、運用Python網路爬蟲、連結SQL資料庫、處理Excel和csv，寄送email，一步步實作成為Python達人！',
+    highlights: [
+      '超過40小時的課程，學習所有必要的Python程式知識！！',
+      '學會基本Python語法、培養程式邏輯以及程式語言觀念',
+      '識別基本與進階Python資料類別與使用時機'
+    ],
+    rating: 4.7,
+    price: 330,
+    created_by: 'John Doe',
+    updated_at: '2024-01-01',
+  }
+];
