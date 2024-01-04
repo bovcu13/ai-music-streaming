bd87f6142d9036590b8cc98fdf20e33db3eb9447
一萱 <jjps30516@gmail.com>
2024-01-04 10:41:40 +0800

    fix: login route

--- src/app/view/page/page.routes.ts
+++ src/app/view/page/page.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { PageComponent } from "./page/page.component";
 import { RegisterComponent } from "./register/register.component";
-import { VideoComponent } from './page/video/video/video.component';
+import { LoginComponent } from "./login/login.component";
 
 export const routes: Routes = [
   {
     path: '', component: PageComponent, children: [
       { path: 'register', component: RegisterComponent },
-
-
+      { path: 'login', component: LoginComponent }
     ]
   },
 ];
