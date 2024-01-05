8db1b557f5a7be4b3293fc14ed0ae0b8af3397bd
一萱 <jjps30516@gmail.com>
2024-01-05 17:16:56 +0800

    feat: course-view

--- src/app/view/container/course/course-view/course-view.component.ts
+++ src/app/view/container/course/course-view/course-view.component.ts
@@ -3,7 +3,9 @@ import { PRIMENG_MODULES } from "../../../../share/primeng";
 import { ActivatedRoute } from "@angular/router";
 import { CourseService } from "../../../../services/course.service";
 import { course } from "../../../../share/data/course";
-import { NgForOf, NgIf } from "@angular/common";
+import { NgClass, NgForOf, NgIf } from "@angular/common";
+
+declare var ColorThief: any;
 
 @Component({
   selector: 'app-course-view',
@@ -11,13 +13,15 @@ import { NgForOf, NgIf } from "@angular/common";
   imports: [
     PRIMENG_MODULES,
     NgForOf,
-    NgIf
+    NgIf,
+    NgClass
   ],
   templateUrl: './course-view.component.html',
   styleUrl: './course-view.component.scss'
 })
 export class CourseViewComponent implements OnInit {
   course: any;
+  mainColor: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,14 +34,36 @@ export class CourseViewComponent implements OnInit {
     this.getCourseData();
   }
 
+  isRotated: boolean[] = [false,false];
+
+  toggleRotate(id: number) {
+    this.isRotated[id] = !this.isRotated[id];
+  }
+
   getCourseData() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id !== null) {
       this.course = this.courseService.getCourseById(id);
+      this.getDominantColor(this.course.imageUrl);
       console.log(this.course);
     } else {
       console.log('id is null');
     }
+  }
+
+  getDominantColor(imageUrl: string) {
+    const colorThief = new ColorThief();
+    const img = new Image();
+
+    let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+    img.crossOrigin = 'Anonymous';
+    img.src = googleProxyURL + encodeURIComponent(imageUrl);
+
+    img.onload = () => {
+      const color = colorThief.getColor(img);
+      this.mainColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.5)`;
+      console.log(this.mainColor);
+    };
   }
 
 }
