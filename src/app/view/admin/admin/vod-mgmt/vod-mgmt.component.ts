9842f619d9362e33ea9a5e3c6f783adb921a1820
一萱 <jjps30516@gmail.com>
2024-01-11 11:42:56 +0800

    feat: admin menu

---
+++ src/app/view/admin/admin/vod-mgmt/vod-mgmt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vod-mgmt',
+  standalone: true,
+  imports: [],
+  templateUrl: './vod-mgmt.component.html',
+  styleUrl: './vod-mgmt.component.scss'
+})
+export class VodMgmtComponent {
+
+}
