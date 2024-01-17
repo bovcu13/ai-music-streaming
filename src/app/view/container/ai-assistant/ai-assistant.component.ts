064b61500f8c56d90c49c252b9078524b5e61cfe
一萱 <jjps30516@gmail.com>
2024-01-17 14:56:47 +0800

    fix: ai-assistant

--- src/app/view/container/ai-assistant/ai-assistant.component.ts
+++ src/app/view/container/ai-assistant/ai-assistant.component.ts
@@ -27,6 +27,7 @@ export class AiAssistantComponent implements OnInit {
   tips = tips;
   messages: any = []
   isTyping = false;
+  typingInterval: any = null;
   thumbsUpStatus = [{}];
   thumbsDownStatus = [{}];
   id = 0;
@@ -45,59 +46,69 @@ export class AiAssistantComponent implements OnInit {
 
   onEnter(event: any) {
     event.preventDefault(); // 防止換行
-    if (this.myForm.controls['searchInput'].value !== '') {
+    if (this.myForm.controls['searchInput'].value !== '' && !this.isTyping) {
       this.onSearchInput();
     }
   }
 
   onSearchInput() {
-    const searchInput = this.myForm.get('searchInput')!.value;
-    if (searchInput == "video") {
-      // 查詢影片
-      this.messages.push({
-        message: searchInput,
-        showVideo: true
-      })
-    } else {
-      // 使用者
-      this.messages.push({
-        id: this.id++,
-        message: searchInput,
-      })
-      this.isTyping = true;
-      this.scrollToBottom();
-      // bot
-      setTimeout(() => {
+    if (!this.isTyping) {
+      const searchInput = this.myForm.get('searchInput')!.value;
+      if (searchInput == "video") {
+        // 查詢影片
+        this.messages.push({
+          message: searchInput,
+          showVideo: true
+        })
+      } else {
+        // 使用者
         this.messages.push({
           id: this.id++,
-          message: '',
-          sender: 'bot'
-        });
-        this.typeMessage('您好，很高興為您服務！😝 <br>其他您可能喜歡的內容：<br><a href="https://reurl.cc/K40V3y">學習應援團 一萬堂熱門課程在家免費學│聯成電腦</a>', 100)
+          message: searchInput,
+        })
         this.scrollToBottom();
-      }, 1000); // 延遲1秒後執行
+        this.isTyping = true;
+        // bot
+        setTimeout(() => {
+          this.messages.push({
+            id: this.id++,
+            message: '',
+            sender: 'bot'
+          });
+          this.typeMessage('您好，很高興為您服務！😝 <br><a href="https://reurl.cc/K40V3y">學習應援團 一萬堂熱門課程在家免費學│聯成電腦</a>', 100)
+          this.scrollToBottom();
+        }, 1000); // 延遲1秒後執行
+      }
+      this.myForm.get('searchInput')!.setValue('');
     }
-
-    this.myForm.get('searchInput')!.setValue('');
   }
 
   typeMessage(message: string, speed: number): Promise<void> {
     return new Promise((resolve) => {
       let i = 0;
-      const interval = setInterval(() => {
+      this.typingInterval = setInterval(() => {
         if (i < message.length) {
           // Append the next character to the message
           this.messages[this.messages.length - 1].message += message[i];
           i++;
         } else {
           // All characters have been appended, clear the interval
-          clearInterval(interval);
+          clearInterval(this.typingInterval);
           this.isTyping = false;
           resolve();
         }
         this.scrollToBottom();
       }, speed);
     });
+  }
+
+  newChat() {
+    this.messages = [];
+    this.isTyping = false;
+    if (this.typingInterval) {
+      clearInterval(this.typingInterval);
+      this.typingInterval = null;
+    }
   }
 
   copy(content: any) {
